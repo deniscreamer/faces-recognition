5f80eab4b1808b8c372f75ad45d9b13d83099d45
Denis Screamer <deni.screamer@gmail.com>
2019-05-11 10:48:10 +0300

    Updates

---
+++ precache-manifest.356b7554052c2b316af9c981ecd3e3d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724804994b3d104b15e444aafff43233",
+    "url": "/faces-recognition/index.html"
+  },
+  {
+    "revision": "760e2ec3ada62328c723",
+    "url": "/faces-recognition/static/css/main.2d72e424.chunk.css"
+  },
+  {
+    "revision": "bfcd9a41084870e0955c",
+    "url": "/faces-recognition/static/js/2.9ff3c2b9.chunk.js"
+  },
+  {
+    "revision": "760e2ec3ada62328c723",
+    "url": "/faces-recognition/static/js/main.e79f6cd1.chunk.js"
+  },
+  {
+    "revision": "d1bfb237c4b2822a1e55",
+    "url": "/faces-recognition/static/js/runtime~main.c84e1919.js"
+  }
+]);
